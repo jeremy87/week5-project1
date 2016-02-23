f606065b7771c495a9bcb440ea3fbd14818e3870
jeremy87 <jeremy.pike87@gmail.com>
2016-02-22 18:46:20 -0700

    homework

--- grocery.js
+++ grocery.js
@@ -17,8 +17,45 @@ $(document).ready(function() {
 // Display each item's name, price, and quantity.
 // Ex: Tomatoes (5) @ $3.99
 
+var list = "";
+        for(i=0; i<groceries.length; i++){
+        var item = groceries[i]	
+        list +="<li>"+item.name + " (" + item.quantity + ") @ $" + item.price +"</li>";
+        }
+    $("#list").append(list);
+
+    function displayTotal(){
+    	var total = 0;
+    	for(i=0; i<groceries.length; i++){
+    		total += (groceries.price * groceries.quantity)
+    	}
+    }
+
 //2. Use the inputs and add button to add grocery items to the beginning of the list.
 // Default status should be "needed". The item should appear above the existing grocery items.
+
+	$(".btn-success").click(function(){
+		var nameInput = $("#addItem").val();
+		var priceInput = $("#addPrice").val();
+		var quantityInput = $("#addQuantity").val();
+		if(nameInput == '' || priceInput == '' || quantityInput == ''){
+			alert("Please fill in all fields")
+		}
+		else {
+			addItem(nameInput, priceInput, quantityInput);
+		}
+	});
+
+	function addItem(name, price, quantity){
+		groceries.unshift({name: name, status: "needed", price: price, quantity: quantity});
+		updateList(groceries[0]);
+		displayTotal();
+	};
+
+	function updateList(item){
+		$("<li>"+ item.name + " (" + item.quantity + ") @ $" + item.price +"</li>")
+		.insertBefore("#list li:first-child")
+	}
 
 //3. Make sure that the grocery list displayed updates when you add an item to the list.
 
